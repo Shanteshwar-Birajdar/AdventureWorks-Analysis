3f9b0d1cc4bbcb9dfac9ab1f8b073ee6e1914e52
Shanteshwar-Birajdar <155803930+Shanteshwar-Birajdar@users.noreply.github.com>
2024-01-06 13:00:48 +0530

    Add result in product_top_10_by_price.sql

--- Products/product_top_10_by_price.sql
+++ Products/product_top_10_by_price.sql
@@ -2,3 +2,19 @@ SELECT product.Name, product.ListPrice
 from product
 order by product.ListPrice desc
 limit 10
+
+
+/* 
+\\\ Result ///
+Name 				            ListPrice
+Road-150 Red, 52	      3578.27
+Road-150 Red, 44	      3578.27
+Road-150 Red, 62	      3578.27
+Road-150 Red, 48	      3578.27
+Road-150 Red, 56	      3578.27
+Mountain-100 Silver, 42	3399.99
+Mountain-100 Silver, 38	3399.99
+Mountain-100 Silver, 44	3399.99
+Mountain-100 Silver, 48	3399.99
+Mountain-100 Black, 38	3374.99
+*/
